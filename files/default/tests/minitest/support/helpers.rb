ab2b2b8fece67c2a4d298ecae83564359a7dd82d
Dave Shawley <daveshawley@gmail.com>
2013-10-06 06:57:27 -0400

    integration helpers: Add File.with_permissions.

--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -24,25 +24,32 @@ module Helpers
       resource
     end
   end
+
+  module ResourceAssertions
+    def with_permissions(permission_set)
+      # Similar to with(:mode, value) except that comparision
+      # is a bitwise AND which is more appropriate than equality.
+
+      # make a `nice' failure message
+      failure_message = "The #{resource_name} #{path} does not have the " +
+        "expected permission set: expected #{permission_set.to_s(8)}, " +
+        "found #{mode}"
+      masked_permissions = (mode.to_i(8) & permission_set)
+      mt = Object.extend(MiniTest::Assertions)
+      mt.assert masked_permissions == permission_set, failure_message
+
+      self  # enable chaining
+    end
+  end
 end
 
 class Chef
   class Resource
     class Directory
-      def with_permissions(permission_set)
-        # Similar to with(:mode, value) except that comparision
-        # is a bitwise AND which is more appropriate than equality.
-        
-        # make a `nice' failure message
-        failure_message = "The #{resource_name} #{path} does not have the " +
-          "expected permission set: expected #{permission_set.to_s(8)}, " +
-          "found #{mode}"
-        masked_permissions = (mode.to_i(8) & permission_set)
-        mt = Object.extend(MiniTest::Assertions)
-        mt.assert masked_permissions == permission_set, failure_message
-
-        self  # enable chaining
-      end
+      include Helpers::ResourceAssertions
+    end
+    class File
+      include Helpers::ResourceAssertions
     end
   end
 end
